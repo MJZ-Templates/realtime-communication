22c66836c3f2540e11a105a4181cffdb455f0f18
mango <mango.koo@goorm.io>
2025-04-08 17:26:28 +0900

    feat: add solveHandler which update the question states
    The solveHandler update the question's check state into true

---
+++ back/src/main/java/arkain/dev/communication/chat/app/service/messageHandler/SolveHandler.java
@@ -0,0 +1,26 @@
+package arkain.dev.communication.chat.app.service.messageHandler;
+
+import arkain.dev.communication.chat.app.ChatMessageHandler;
+import arkain.dev.communication.chat.app.dto.ChatMessageDto;
+import arkain.dev.communication.chat.app.dto.ChatResponseMessage;
+import arkain.dev.communication.chat.app.service.ChatMessageService;
+import arkain.dev.communication.chat.dao.entity.MessageType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SolveHandler implements ChatMessageHandler {
+
+    private final ChatMessageService chatMessageService;
+
+    @Override
+    public boolean supports(MessageType type) {
+        return MessageType.SOLVE == type;
+    }
+
+    @Override
+    public ChatResponseMessage handleMessage(String roomId, ChatMessageDto message) {
+        return chatMessageService.solveProblem(message);
+    }
+}
